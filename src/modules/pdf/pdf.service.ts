f45b903a1b8170943c68ba4ff3e06bd24d41b66e
Rider Mamani <rdrmamani@gmail.com>
2026-01-11 23:55:41 -0400

    refactor: se cambio de pdfmake sintaxis 0.2.x a 0.3.x

--- src/modules/pdf/pdf.service.ts
+++ src/modules/pdf/pdf.service.ts
@@ -1,28 +1,39 @@
+/* eslint-disable */
 import { Injectable } from '@nestjs/common';
+import { rejects } from 'node:assert';
 import { join } from 'node:path';
-import PdfMake from 'pdfmake/build/pdfmake';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import PdfInstance from 'pdfmake';
 
 const fonts = {
   Roboto: {
-    normal: join(__dirname, '../..', 'resources/fonts/Roboto/Roboto-Regular.ttf'),
-    bold: join(__dirname, '../..', 'resources/fonts/Roboto/Roboto-Medium.ttf'),
-    italics: join(__dirname, '../..', 'resources/fonts/Roboto/Roboto-Italic.ttf'),
-    bolditalics: join(__dirname, '../..', 'resources/fonts/Roboto/Roboto-MediumItalic.ttf'),
+    normal: join(__dirname, '../../resources/fonts/Roboto/Roboto-Regular.ttf'),
+    bold: join(__dirname, '../../resources/fonts/Roboto/Roboto-Medium.ttf'),
+    italics: join(__dirname, '../../resources/fonts/Roboto/Roboto-Italic.ttf'),
+    bolditalics: join(__dirname, '../../resources/fonts/Roboto/Roboto-MediumItalic.ttf'),
   },
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-(PdfMake as any).addFonts(fonts);
-
 @Injectable()
 export class PdfService {
-  constructor() {}
+  private readonly pdfMake: typeof PdfInstance;
+  constructor() {
+    this.pdfMake = PdfInstance;
+    this.pdfMake.addFonts(fonts);
+  }
 
-  async generarPdf(docDefinition: TDocumentDefinitions): Promise<Buffer> {
-    const buffer: Buffer = await new Promise((resolve) => {
-      PdfMake.createPdf(docDefinition).getBuffer((b: Buffer) => resolve(b));
+  generarPdf(docDefinition): Promise<Buffer> {
+    return new Promise((resolve) => {
+      this.pdfMake
+        .createPdf(docDefinition)
+        .getBuffer()
+        .then(
+          (binary) => {
+            resolve(binary);
+          },
+          (err) => {
+            console.error(err);
+          },
+        );
     });
-    return buffer;
   }
 }
